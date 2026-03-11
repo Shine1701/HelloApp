02c11e2bd513ddc5d9dbddd219e4ab14f0550126
Shine1701 <nathshine2007@gmail.com>
2026-03-11 20:10:18 +0530

    Implemented UC3-Display Hello World

--- HelloApp(UC3).java
+++ HelloApp(UC3).java
@@ -1,14 +1,11 @@
-public class HelloWorld {
+public class Hello {
     public static void main(String[] args) {
 
-        String name;
-
         if (args.length > 0) {
-            name = args[0];
+            System.out.println("Hello " + args[0]);
         } else {
-            name = "World";
+            System.out.println("Hello World");
         }
 
-        System.out.println("Hello " + name);
     }
 }
