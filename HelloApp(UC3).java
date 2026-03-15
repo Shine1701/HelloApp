da9de9c982f05a7b4fa2fdc14ed5b701631b1ff7
Shine1701 <nathshine2007@gmail.com>
2026-03-15 22:33:04 +0530

    Resolved

--- HelloApp(UC3).java
+++ HelloApp(UC3).java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -13,4 +14,19 @@ int main(int argc, char* argv[])
     }
 
     return 0;
+=======
+class HelloApp
+{
+    public static void main(String[] args) 
+    {
+        if(args.length > 0) 
+        {
+            System.out.println("Hello " + args[0]);
+        } 
+        else 
+        {
+            System.out.println("Hello World");
+        }
+    }
+>>>>>>> feature/UC3
 }
