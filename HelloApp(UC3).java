2d7811a42c914f3834c5109b6fad3b26ef5d3e36
Shine1701 <nathshine2007@gmail.com>
2026-03-15 19:57:00 +0530

    Display Hello with command line name or default World

--- HelloApp(UC3).java
+++ HelloApp(UC3).java
@@ -1,11 +1,16 @@
-public class Hello {
-    public static void main(String[] args) {
+#include <iostream>
+using namespace std;
 
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello World");
-        }
-
+int main(int argc, char* argv[])
+{
+    if(argc > 1)
+    {
+        cout << "Hello " << argv[1] << endl;
     }
+    else
+    {
+        cout << "Hello World" << endl;
+    }
+
+    return 0;
 }
