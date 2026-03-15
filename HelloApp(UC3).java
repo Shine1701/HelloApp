8f9e5526e0063c8a887c0af97af4ad279061cb2f
Shine1701 <nathshine2007@gmail.com>
2026-03-15 20:14:17 +0530

    display hello world uc3

---
+++ HelloApp(UC3).java
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if(argc > 1)
+    {
+        cout << "Hello " << argv[1] << endl;
+    }
+    else
+    {
+        cout << "Hello World" << endl;
+    }
+
+    return 0;
+}
